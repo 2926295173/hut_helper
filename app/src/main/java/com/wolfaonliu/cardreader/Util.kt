1c06f59c8b0f375196dd43b0ba4c72256762b01b
aoian5173 <2926295173@qq.com>
2022-04-10 23:06:25 +0800

    6

--- app/src/main/java/com/wolfaonliu/cardreader/Util.kt
+++ app/src/main/java/com/wolfaonliu/cardreader/Util.kt
@@ -1,86 +1,88 @@
-package com.wolfaonliu.cardreader;
+package com.wolfaonliu.cardreader
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.widget.Toast;
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.widget.Toast
+import java.util.regex.Pattern
 
-import java.util.regex.Pattern;
-
-
-public class Util {
+object Util {
     //弹窗
-    public static void aToast(String string, Activity activity) {
-        Toast.makeText(activity, string, Toast.LENGTH_SHORT).show();
+    @JvmStatic
+    fun aToast(string: String?, activity: Activity?) {
+        Toast.makeText(activity, string, Toast.LENGTH_SHORT).show()
     }
 
     //字节转换成16进制
-    public static String byteToHex(byte[] bArr) {
-        int i = 0;
-        String[] strArr = new String[]
-                {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
-        StringBuilder str = new StringBuilder();
-        while (i < bArr.length) {
-            int i2 = bArr[i] & 0xFF;
-            str = new StringBuilder((str + strArr[(i2 >> 4) & 0xF]) + strArr[i2 & 0xF]);
-            i++;
+    @JvmStatic
+    fun byteToHex(bArr: ByteArray): String {
+        var i = 0
+        val strArr =
+            arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F")
+        var str = StringBuilder()
+        while (i < bArr.size) {
+            val i2: Int = bArr[i].toInt() and 0xFF
+            str = StringBuilder(str.toString() + strArr[i2 shr 4 and 0xF] + strArr[i2 and 0xF])
+            i++
         }
-        return str.toString();
+        return str.toString()
     }
 
-    public static boolean unicodeBlockOfZ(String str) {
-        char[] toCharArray = Pattern.compile("\\s*|\t*|\r*|\n*").matcher(str).replaceAll("").replaceAll("\\p{P}", "").trim().toCharArray();
-        float length = (float) toCharArray.length;
-        float f = 0.0f;
-        for (char c : toCharArray) {
-            if (!(Character.isLetterOrDigit(c) || unicodeBlockOfZ(c))) {
-                f += 1.0f;
+    fun unicodeBlockOfZ(str: String?): Boolean {
+        val toCharArray = Pattern.compile("\\s*|\t*|\r*|\n*").matcher(str).replaceAll("")
+            .replace("\\p{P}".toRegex(), "").trim { it <= ' ' }
+            .toCharArray()
+        val length = toCharArray.size.toFloat()
+        var f = 0.0f
+        for (c in toCharArray) {
+            if (!(Character.isLetterOrDigit(c) || unicodeBlock(c))) {
+                f += 1.0f
             }
         }
-        return ((double) (f / length)) > 0.4d;
+        return (f / length).toDouble() > 0.4
     }
 
-    public static boolean unicodeBlockOfZ(char c) {
-        Character.UnicodeBlock of = Character.UnicodeBlock.of(c);
-        boolean o = (of == Character.UnicodeBlock.GENERAL_PUNCTUATION);
-        boolean p = (of == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION);
-        boolean q = (of == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS);
-        boolean x = (of == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS);
-        boolean y = (of == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS);
-        boolean z = (of == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A);
-        return x || y || z || o || p || q;
+    private fun unicodeBlock(c: Char): Boolean {
+        val of = Character.UnicodeBlock.of(c)
+        val o = of === Character.UnicodeBlock.GENERAL_PUNCTUATION
+        val p = of === Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+        val q = of === Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+        val x = of === Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+        val y = of === Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+        val z = of === Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+        return x || y || z || o || p || q
     }
 
-    public static boolean isNone2(String str) {
-        boolean x = (str == null);
-        boolean y = (str.trim().equals(""));
-        return x || y;
+    @JvmStatic
+    fun isNone2(str: String?): Boolean {
+        val x = str == null
+        val y = str!!.trim { it <= ' ' } == ""
+        return x || y
     }
 
     //判断有数据
-    public static boolean isNotNone3(String str) {
-        boolean x = (str == null);
-        boolean y = (str.trim().equals(""));
-        boolean z = (str.equalsIgnoreCase("null"));
-        return !(x || y || z);
-
+    @JvmStatic
+    fun isNotNone3(str: String?): Boolean {
+        val x = str == null
+        val y = str!!.trim { it <= ' ' } == ""
+        val z = str.equals("null", ignoreCase = true)
+        return !(x || y || z)
     }
 
     //获取版本号
-    public static String getVersion(Context context) {
-        String ver = "";
+    @JvmStatic
+    fun getVersion(context: Context): String {
+        var ver = ""
         try {
-            PackageManager packageManager = context.getPackageManager();
-            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
-            ver = packageInfo.versionName;
-            if (ver.equals("") || ver.length() <= 0) {
-                return "";
+            val packageManager = context.packageManager
+            val packageInfo = packageManager.getPackageInfo(context.packageName, 0)
+            ver = packageInfo.versionName
+            if (ver == "" || ver.isEmpty()) {
+                return ""
             }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
         }
-        return ver;
+        return ver
     }
-
 }
