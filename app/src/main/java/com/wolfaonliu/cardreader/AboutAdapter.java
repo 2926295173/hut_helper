e1c9c9e3ca729e43e15f2c009c3ba93325f803d3
aoian5173 <2926295173@qq.com>
2022-04-10 22:24:48 +0800

    6

--- app/src/main/java/com/wolfaonliu/cardreader/AboutAdapter.java
+++ app/src/main/java/com/wolfaonliu/cardreader/AboutAdapter.java
@@ -9,17 +9,10 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/**
- * Created by Mogician on 2018/3/14.
- */
-
 public class AboutAdapter extends BaseAdapter {
 
-    private ArrayList<String[]> mData;
-    private Activity activity;
-
-    private TextView setting;
-    private TextView settingsub;
+    private final ArrayList<String[]> mData;
+    private final Activity activity;
 
 
     public AboutAdapter(ArrayList<String[]> data, Activity activity) {
@@ -46,8 +39,8 @@ public class AboutAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         convertView = LayoutInflater.from(activity).inflate(R.layout.about_item, parent, false);
 
-        setting = convertView.findViewById(R.id.setting);
-        settingsub = convertView.findViewById(R.id.settingsub);
+        TextView setting = convertView.findViewById(R.id.setting);
+        TextView settingsub = convertView.findViewById(R.id.settingsub);
 
 //
         setting.setText(mData.get(position)[0]);
