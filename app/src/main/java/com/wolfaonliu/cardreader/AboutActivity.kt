3d49fc80ceeff994e4d994fafb9cc724612e12d9
aoian5173 <2926295173@qq.com>
2022-04-12 21:49:51 +0800

    ok1

--- app/src/main/java/com/wolfaonliu/cardreader/AboutActivity.kt
+++ app/src/main/java/com/wolfaonliu/cardreader/AboutActivity.kt
@@ -1,103 +1,99 @@
-package com.wolfaonliu.cardreader;
+package com.wolfaonliu.cardreader
 
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.wolfaonliu.cardreader.Util.getVersion
+import java.util.*
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class AboutActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-
-    Toolbar toolbar;
-
-
-    private AboutAdapter mAdapter;
-
-    private RecyclerView.LayoutManager mLayoutManager;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-
-        toolbar = findViewById(R.id.about_toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationOnClickListener(v -> finish());
-        initData();
-        initView();
+class AboutActivity : AppCompatActivity(), OnItemClickListener {
+    private var toolbar: Toolbar? = null
+    private var mAdapter: AboutAdapter? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        toolbar = findViewById(R.id.about_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toolbar?.setNavigationOnClickListener(View.OnClickListener { finish() })
+        initData()
+        initView()
     }
 
-
-    private void initData() {
-        mAdapter = new AboutAdapter(getSet(), this);
+    private fun initData() {
+        mAdapter = AboutAdapter(set, this)
     }
 
-    private ArrayList<String[]> getSet() {
-        ArrayList<String[]> settings = new ArrayList<>();
-        String[][] s = new String[4][2];
-        s[0][0] = getString(R.string.version);
-        s[0][1] = Util.getVersion(this.getApplicationContext());
-        s[1][0] = getString(R.string.developer);
-        s[1][1] = "aoian";
-        s[2][0] = getString(R.string.contact);
-        s[2][1] = "wolfaonliuxgmailcom";
-        s[3][0] = getString(R.string.github);
-        s[3][1] = "https://github.com/2926295173/hut_helper";
-        settings.addAll(Arrays.asList(s));
-        return settings;
-    }
+    private val set: ArrayList<Array<String>>
+        get() {
+            val settings = ArrayList<Array<String>>()
+            val s = Array(4) { arrayOfNulls<String>(2) }
+            s[0][0] = getString(R.string.version)
+            s[0][1] = getVersion(this.applicationContext)
+            s[1][0] = getString(R.string.developer)
+            s[1][1] = "aoian"
+            s[2][0] = getString(R.string.contact)
+            s[2][1] = "wolfaonliuxgmailcom"
+            s[3][0] = getString(R.string.github)
+            s[3][1] = "https://github.com/2926295173/hut_helper"
+            val sss = s[2][1]
+            val array1 = arrayOf("版本号", getVersion(this.applicationContext), "开发者", "aoian")
+            val x = mutableListOf(array1)
+            //出现了为空的问题，暂时不导入整个列表组了
+            //val x = mutableListOf<Array<String>>(*s)
+            settings.addAll(x)
 
-    private void initView() {
-        ListView mView = findViewById(R.id.set_list);
-        // 设置adapter
-        mView.setAdapter(mAdapter);
-        mView.setOnItemClickListener(this);
-    }
+            return settings
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Uri uri;
-        Intent intent;
-        switch (position) {
-            case 2:
-                showEmailDialog();
-                break;
-            case 3:
-                uri = Uri.parse("https://github.com/2926295173/hut_helper");
-                intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-                break;
-            default:
-                break;
+
         }
-        Toast.makeText(this, "你点击了第" + position + "项", Toast.LENGTH_SHORT).show();
+
+    private fun initView() {
+        val mView = findViewById<ListView>(R.id.set_list)
+        // 设置adapter
+        mView.adapter = mAdapter
+        mView.onItemClickListener = this
     }
 
+    override fun onItemClick(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+        val uri: Uri
+        val intent: Intent
+        when (position) {
+            2 -> showEmailDialog()
+            3 -> {
+                uri = Uri.parse("https://github.com/2926295173/hut_helper")
+                intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
+            }
+            else -> {}
+        }
+        Toast.makeText(this, "你点击了第" + position + "项", Toast.LENGTH_SHORT).show()
+    }
 
-    private void showEmailDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    private fun showEmailDialog() {
+        val builder = AlertDialog.Builder(this)
         builder.setTitle(R.string.Report)
-                .setMessage(R.string.report_atten);
-        builder.setPositiveButton(R.string.yes, (dialog, which) -> {
-            Intent email = new Intent(Intent.ACTION_SEND);
-            email.setType("message/rfc822");
-            email.putExtra(Intent.EXTRA_EMAIL, new String[]{"wolfaonliuxgmailcom"});
-            email.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name) + getString(R.string.report));
-            startActivity(Intent.createChooser(email, getString(R.string.email_clint)));
-        });
-        builder.setNegativeButton(R.string.no, null);
-
-        builder.show();
+            .setMessage(R.string.report_atten)
+        builder.setPositiveButton(R.string.yes) { dialog: DialogInterface?, _: Int ->
+            val email = Intent(Intent.ACTION_SEND)
+            email.type = "message/rfc822"
+            email.putExtra(Intent.EXTRA_EMAIL, arrayOf("wolfaonliuxgmailcom"))
+            email.putExtra(
+                Intent.EXTRA_SUBJECT,
+                getString(R.string.app_name) + getString(R.string.report)
+            )
+            startActivity(Intent.createChooser(email, getString(R.string.email_clint)))
+        }
+        builder.setNegativeButton(R.string.no, null)
+        builder.show()
     }
 }
