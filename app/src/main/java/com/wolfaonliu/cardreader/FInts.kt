9aced30125f9a383a0b137faa72e6a861b67f09b
aoian5173 <2926295173@qq.com>
2022-04-16 20:08:48 +0800

    ok

--- app/src/main/java/com/wolfaonliu/cardreader/FInts.kt
+++ app/src/main/java/com/wolfaonliu/cardreader/FInts.kt
@@ -3,11 +3,11 @@ package com.wolfaonliu.cardreader
 object FInts {
     //const val a = "00A404000E4E432E65436172642E4444463031"
 
-    const val req = "00A40000023F00"
-    const val rats = "00A4000002EC01"
-    const val balance = "00A4000002EC11"
+    const val req = "00A40000023F00"//[B@26a1ab54
+    const val rats = "00A4000002EC01"//[B@26a1ab54
+    const val balance = "00A4000002EC11"//[B@26a1ab54
 
-    const val xueyuan_id = "00B0962F3E"
+    const val xueyuan_id = "00B0962F3E" //[b@26a1ab54
     const val card_id = "00B0968414"
 
     const val stu_name = "00B0960214"
